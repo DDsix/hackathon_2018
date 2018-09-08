27f26ca288da129466d60d7e0e39f3d7e89a847e
Daniel Popovici <daniel.popovici@emag.ro>
2018-09-09 01:00:49 +0300

    Added FCM support

--- app/src/main/java/com/example/danielpopovici/giftfinder/WishlistActivity.kt
+++ app/src/main/java/com/example/danielpopovici/giftfinder/WishlistActivity.kt
@@ -3,6 +3,9 @@ package com.example.danielpopovici.giftfinder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessagingService
 import kotlinx.android.synthetic.main.activity_wishlist.*
 
 class WishlistActivity : AppCompatActivity() {
@@ -16,6 +19,8 @@ class WishlistActivity : AppCompatActivity() {
         title = "Gift finder"
 
         setupViewPager()
+
+        Log.d("FCM_TOKEN:", FirebaseInstanceId.getInstance().token)
     }
 
     private fun setupViewPager() {

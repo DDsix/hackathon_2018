cf46346af6e1f3e6c82dcfe5946e1e53c0d77535
Daniel Popovici <daniel.popovici@emag.ro>
2018-09-08 17:09:12 +0300

    More mocked data

--- app/src/main/java/com/example/danielpopovici/giftfinder/MockApi.kt
+++ app/src/main/java/com/example/danielpopovici/giftfinder/MockApi.kt
@@ -9,7 +9,7 @@ fun getFacebookFriends() = mutableListOf<SocialFriend>().apply {
 
     add(SocialFriend(1,
             "George Marian",
-            ""))
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr-8Ub8YcIdWTpG8ntlYBFh2MjLtjY-14krlZV_4n8UTIVLTzI"))
 
     add(SocialFriend(2,
             "George Bunea",
@@ -24,6 +24,32 @@ fun getFacebookFriends() = mutableListOf<SocialFriend>().apply {
             "https://ak8.picdn.net/shutterstock/videos/1583458/thumb/3.jpg"))
 
     add(SocialFriend(5,
+            "Radu Ene",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAPVGMEgzwNWXzN8KRscyBpJXo655IRzxUBrAobSf6dlwIUgf3"))
+
+
+    add(SocialFriend(6,
+            "Madalina Loghin",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQStarbhWvO65tb2lRjVuXaeMtYc-Yy6H0o3CIqmcva8MixvH3k"))
+
+
+    add(SocialFriend(7,
+            "George Marian",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr-8Ub8YcIdWTpG8ntlYBFh2MjLtjY-14krlZV_4n8UTIVLTzI"))
+
+    add(SocialFriend(8,
+            "George Bunea",
+            "https://thewellspringfield.org/wp-content/uploads/2016/02/stephen-rippee-the-well-church-springfield-mo_web.jpg"))
+
+    add(SocialFriend(9,
+            "Raluca Bodea",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNsJR6eLYg876E58tKsPlQoRTz7tX5ZFhZ6PgPj9Jsr4FqBIFi"))
+
+    add(SocialFriend(10,
+            "Daniel Popovici",
+            "https://ak8.picdn.net/shutterstock/videos/1583458/thumb/3.jpg"))
+
+    add(SocialFriend(11,
             "Radu Ene",
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAPVGMEgzwNWXzN8KRscyBpJXo655IRzxUBrAobSf6dlwIUgf3"))
 

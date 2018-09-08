163d416846e478e2c82a4ee3b71fd11836271ed3
madalinaloghin <loghinmariamadalina@gmail.com>
2018-09-08 22:20:15 +0300

    design updates

--- app/src/main/java/com/example/danielpopovici/giftfinder/FragmentMyWishlists.kt
+++ app/src/main/java/com/example/danielpopovici/giftfinder/FragmentMyWishlists.kt
@@ -12,6 +12,7 @@ class FragmentMyWishlists : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_my_wishlists, container, false)
 
+        view.customRecommendation0.setProducts("Toate produsele favorite", getAllWishlistItems())
 
         getSingleWishlistItems(0).apply {
             view.customRecommendation1.setProducts(name, products)

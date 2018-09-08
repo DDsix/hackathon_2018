f01b95a6318f5599ed97899934e33fa0efe11555
madalinaloghin <loghinmariamadalina@gmail.com>
2018-09-08 17:59:23 +0300

    Wishlist and Product
    Wishlist and Product

---
+++ app/src/main/java/com/example/danielpopovici/giftfinder/Wishlist.kt
@@ -0,0 +1,8 @@
+package com.example.danielpopovici.giftfinder
+
+/**
+ * @author madalina.loghin on 9/8/2018.
+ */
+class Wishlist(val id: Int,
+               val name: String,
+               val products: List<Product>)

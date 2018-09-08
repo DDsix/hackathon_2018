307cdc1bcf6a8e1fd8c6fffe2c1b518f890d62d8
madalinaloghin <loghinmariamadalina@gmail.com>
2018-09-08 18:09:52 +0300

    mock products for wishlists

--- app/src/main/java/com/example/danielpopovici/giftfinder/MockApi.kt
+++ app/src/main/java/com/example/danielpopovici/giftfinder/MockApi.kt
@@ -64,7 +64,126 @@ fun getFacebookFriends() = mutableListOf<SocialFriend>().apply {
             "Radu Ene",
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAPVGMEgzwNWXzN8KRscyBpJXo655IRzxUBrAobSf6dlwIUgf3",
             2))
+}
 
+fun getAllWishlistItems() = mutableListOf<Product>().apply {
 
+    add(Product(0, "Nike, Pantofi din tricot pentru alergare FLEX 2017, Violet prafuit/Albastru azur",
+            "https://s12emagst.akamaized.net/products/12675/12674310/images/res_4c3fd4f2bbffdf4abb246792264f0dbe_450x450_g9ko.jpg?v2",
+            379.00))
+    add(Product(1, "Televizor LED NEI, 109 cm, 43NE6000, 4K Ultra HD ",
+            "https://s12emagst.akamaized.net/products/15236/15235626/images/res_18f115850d163be5e23c83a49486ca70_450x450_5bre.png",
+            999.00))
+    add(Product(2, "Nike, Pantofi cu logo, pentru alergare Quest",
+            "https://s12emagst.akamaized.net/products/15542/15541089/images/res_4c1129204287450638687dc582fed8d8_450x450_bc91.jpg?v2",
+            340.00))
+    add(Product(3, "Rucsac laptop Lenovo Classic by NAVA, 15.6\", Negru ",
+            "https://s12emagst.akamaized.net/products/16317/16316908/images/res_89154a860aca7d39bf919a0988106451_450x450_3c0.jpg",
+            199.99))
+    add(Product(4, "Mouse optic ASUS WT465, Wireless, USB, Alb ",
+            "https://s12emagst.akamaized.net/products/4976/4975264/images/res_cb59f8f18cd633bbaade749f734f2571_450x450_dvt9.jpg",
+            99.90))
+    add(Product(5, "Nike, Pantofi din tricot pentru alergare FLEX 2017, Violet prafuit/Albastru azur",
+            "https://s12emagst.akamaized.net/products/12675/12674310/images/res_4c3fd4f2bbffdf4abb246792264f0dbe_450x450_g9ko.jpg?v2",
+            379.00))
+    add(Product(6, "Televizor LED NEI, 109 cm, 43NE6000, 4K Ultra HD ",
+            "https://s12emagst.akamaized.net/products/15236/15235626/images/res_18f115850d163be5e23c83a49486ca70_450x450_5bre.png",
+            999.00))
+    add(Product(7, "Nike, Pantofi cu logo, pentru alergare Quest",
+            "https://s12emagst.akamaized.net/products/15542/15541089/images/res_4c1129204287450638687dc582fed8d8_450x450_bc91.jpg?v2",
+            340.00))
+    add(Product(8, "Rucsac laptop Lenovo Classic by NAVA, 15.6\", Negru ",
+            "https://s12emagst.akamaized.net/products/16317/16316908/images/res_89154a860aca7d39bf919a0988106451_450x450_3c0.jpg",
+            199.99))
+    add(Product(9, "Mouse optic ASUS WT465, Wireless, USB, Alb ",
+            "https://s12emagst.akamaized.net/products/4976/4975264/images/res_cb59f8f18cd633bbaade749f734f2571_450x450_dvt9.jpg",
+            99.90))
+    add(Product(10, "Periuta de dinti electrica Oral-B PRO 2 2000 Cross Action,",
+            "https://s12emagst.akamaized.net/products/2504/2503696/images/res_4c8f544bd6c367570b825f0d8ed69512_full.jpg",
+            279.00))
+    add(Product(11, "Troler Samsonite Base Boost, Red, 55 cm ",
+            "https://s12emagst.akamaized.net/products/8747/8746626/images/res_87850b4b7bfdc28aa936869a6f997309_450x450_405n.jpg",
+            479.00))
+    add(Product(12, "Pachet Philips Sonicare HX6972/34, 2 periute de dinti electrice",
+            "https://s12emagst.akamaized.net/products/1008/1007771/images/res_a35999eb6a5c2d9330a8907370923cf2_full.jpg",
+            979.00))
+    add(Product(13, "Apa de Parfum Lancome La Vie Est Belle, Femei, 75ml",
+            "https://s12emagst.akamaized.net/products/650/649574/images/res_8e57460d0586c5d5ddbab94a416d0285_full.jpg",
+            350.00))
+}
 
+fun getSingleWishlistItems(listId: Int): Wishlist {
+    return when (listId) {
+        0 -> {
+            val items = mutableListOf<Product>().apply {
+                add(Product(0, "Nike, Pantofi din tricot pentru alergare FLEX 2017, Violet prafuit/Albastru azur",
+                        "https://s12emagst.akamaized.net/products/12675/12674310/images/res_4c3fd4f2bbffdf4abb246792264f0dbe_450x450_g9ko.jpg?v2",
+                        379.00))
+                add(Product(2, "Nike, Pantofi cu logo, pentru alergare Quest",
+                        "https://s12emagst.akamaized.net/products/15542/15541089/images/res_4c1129204287450638687dc582fed8d8_450x450_bc91.jpg?v2",
+                        340.00))
+                add(Product(0, "Nike, Pantofi din tricot pentru alergare FLEX 2017, Violet prafuit/Albastru azur",
+                        "https://s12emagst.akamaized.net/products/12675/12674310/images/res_4c3fd4f2bbffdf4abb246792264f0dbe_450x450_g9ko.jpg?v2",
+                        379.00))
+                add(Product(2, "Nike, Pantofi cu logo, pentru alergare Quest",
+                        "https://s12emagst.akamaized.net/products/15542/15541089/images/res_4c1129204287450638687dc582fed8d8_450x450_bc91.jpg?v2",
+                        340.00))
+            }
+            Wishlist(0, "Papuci", items)
+        }
+        1 -> {
+            val items = mutableListOf<Product>().apply {
+                add(Product(1, "Televizor LED NEI, 109 cm, 43NE6000, 4K Ultra HD ",
+                        "https://s12emagst.akamaized.net/products/15236/15235626/images/res_18f115850d163be5e23c83a49486ca70_450x450_5bre.png",
+                        999.00))
+                add(Product(3, "Rucsac laptop Lenovo Classic by NAVA, 15.6\", Negru ",
+                        "https://s12emagst.akamaized.net/products/16317/16316908/images/res_89154a860aca7d39bf919a0988106451_450x450_3c0.jpg",
+                        199.99))
+                add(Product(6, "Televizor LED NEI, 109 cm, 43NE6000, 4K Ultra HD ",
+                        "https://s12emagst.akamaized.net/products/15236/15235626/images/res_18f115850d163be5e23c83a49486ca70_450x450_5bre.png",
+                        999.00))
+                add(Product(1, "Televizor LED NEI, 109 cm, 43NE6000, 4K Ultra HD ",
+                        "https://s12emagst.akamaized.net/products/15236/15235626/images/res_18f115850d163be5e23c83a49486ca70_450x450_5bre.png",
+                        999.00))
+                add(Product(3, "Rucsac laptop Lenovo Classic by NAVA, 15.6\", Negru ",
+                        "https://s12emagst.akamaized.net/products/16317/16316908/images/res_89154a860aca7d39bf919a0988106451_450x450_3c0.jpg",
+                        199.99))
+                add(Product(6, "Televizor LED NEI, 109 cm, 43NE6000, 4K Ultra HD ",
+                        "https://s12emagst.akamaized.net/products/15236/15235626/images/res_18f115850d163be5e23c83a49486ca70_450x450_5bre.png",
+                        999.00))
+            }
+            Wishlist(0, "Electronice", items)
+        }
+
+        else -> {
+            val items = mutableListOf<Product>().apply {
+                add(Product(10, "Periuta de dinti electrica Oral-B PRO 2 2000 Cross Action,",
+                        "https://s12emagst.akamaized.net/products/2504/2503696/images/res_4c8f544bd6c367570b825f0d8ed69512_full.jpg",
+                        279.00))
+                add(Product(11, "Troler Samsonite Base Boost, Red, 55 cm ",
+                        "https://s12emagst.akamaized.net/products/8747/8746626/images/res_87850b4b7bfdc28aa936869a6f997309_450x450_405n.jpg",
+                        479.00))
+                add(Product(12, "Pachet Philips Sonicare HX6972/34, 2 periute de dinti electrice",
+                        "https://s12emagst.akamaized.net/products/1008/1007771/images/res_a35999eb6a5c2d9330a8907370923cf2_full.jpg",
+                        979.00))
+                add(Product(13, "Apa de Parfum Lancome La Vie Est Belle, Femei, 75ml",
+                        "https://s12emagst.akamaized.net/products/650/649574/images/res_8e57460d0586c5d5ddbab94a416d0285_full.jpg",
+                        350.00))
+                add(Product(10, "Periuta de dinti electrica Oral-B PRO 2 2000 Cross Action,",
+                        "https://s12emagst.akamaized.net/products/2504/2503696/images/res_4c8f544bd6c367570b825f0d8ed69512_full.jpg",
+                        279.00))
+                add(Product(11, "Troler Samsonite Base Boost, Red, 55 cm ",
+                        "https://s12emagst.akamaized.net/products/8747/8746626/images/res_87850b4b7bfdc28aa936869a6f997309_450x450_405n.jpg",
+                        479.00))
+                add(Product(12, "Pachet Philips Sonicare HX6972/34, 2 periute de dinti electrice",
+                        "https://s12emagst.akamaized.net/products/1008/1007771/images/res_a35999eb6a5c2d9330a8907370923cf2_full.jpg",
+                        979.00))
+                add(Product(13, "Apa de Parfum Lancome La Vie Est Belle, Femei, 75ml",
+                        "https://s12emagst.akamaized.net/products/650/649574/images/res_8e57460d0586c5d5ddbab94a416d0285_full.jpg",
+                        350.00))
+
+            }
+            Wishlist(0, "Altele", items)
+        }
+    }
 }
